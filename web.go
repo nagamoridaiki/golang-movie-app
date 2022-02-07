2786ab3c7fb38f2c2792d8a2008394af507e03fe
nagamoridaiki <user@USERnoMacBook-puro.local>
2022-02-07 22:00:28 +0900

    パラメータをつけてみた

--- web.go
+++ web.go
@@ -52,29 +52,25 @@ func index(w http.ResponseWriter, rq *http.Request, tmp *template.Template) {
 }
 
 // hello handler.
-var flg bool = true
-
-// hello handler.
 func hello(w http.ResponseWriter, rq *http.Request,
 	tmp *template.Template) {
 
+	id := rq.FormValue("id")
+	nm := rq.FormValue("name")
+	msg := "id: " + id + ", Name: " + nm
+
 	item := struct {
-		Flg      bool
-		Title    string
-		Message  string
-		JMessage string
+		Title   string
+		Message string
 	}{
-		Flg:      flg,
-		Title:    "Send values",
-		Message:  "This is Sample message.",
-		JMessage: "これはサンプルです。",
+		Title:   "Send values",
+		Message: msg,
 	}
 
 	er := tmp.Execute(w, item)
 	if er != nil {
 		log.Fatal(er)
 	}
-	flg = !flg
 }
 
 // main program.
