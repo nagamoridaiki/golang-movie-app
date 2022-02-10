b9b9047a5214b25e5dd591b0eb64972b6091286c
nagamoridaiki <user@USERnoMacBook-puro.local>
2022-02-10 20:00:30 +0900

    セッションの活用

--- web.go
+++ web.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"net/http"
 	"text/template"
+
+	"github.com/gorilla/sessions"
 )
 
 // Temps is template structure.
@@ -51,22 +53,38 @@ func index(w http.ResponseWriter, rq *http.Request, tmp *template.Template) {
 	}
 }
 
+var cs *sessions.CookieStore = sessions.NewCookieStore([]byte("secret-key-12345"))
+
 // hello handler.
 func hello(w http.ResponseWriter, rq *http.Request,
 	tmp *template.Template) {
-	msg := "type name and password:"
+	msg := "login name & password:"
+
+	ses, _ := cs.Get(rq, "hello-session")
 
 	if rq.Method == "POST" {
+		ses.Values["login"] = nil
+		ses.Values["name"] = nil
 		nm := rq.PostFormValue("name")
 		pw := rq.PostFormValue("pass")
-		msg = "name: " + nm + ", passowrd: " + pw
+		if nm == pw {
+			ses.Values["login"] = true
+			ses.Values["name"] = nm
+		}
+		ses.Save(rq, w)
+	}
+
+	flg, _ := ses.Values["login"].(bool)
+	lname, _ := ses.Values["name"].(string)
+	if flg {
+		msg = "logined: " + lname
 	}
 
 	item := struct {
 		Title   string
 		Message string
 	}{
-		Title:   "Send values",
+		Title:   "Session",
 		Message: msg,
 	}
 
